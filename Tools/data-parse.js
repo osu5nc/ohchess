f1de24c37dff8214dae0fd74c6afd06b2b7be2eb
osu5nc <osu5nc@gmail.com>
2019-10-07 22:41:28 -0400

    Updating top 50 lists

--- Tools/data-parse.js
+++ Tools/data-parse.js
@@ -59,60 +59,36 @@ const parseOcaMembers = (data) => {
 	console.log(html);
 };
 
-Papa.parse(`1,"WANG,JASON",2422
-2,"SEDLAR,WILLIAM JO",2404
-3,"SHEN,CHRISTOPHER",2390
-4,"BLOCKER,CALVIN",2384
-5,"SHRIWAS,PRATIK",2379
-6,"GRIGGS,WALKER KYL",2376
-7,"BOOR,CARL",2368
-8,"WANG,EIGEN",2366
-9,"JAYARAMAN,ARVIND",2352
-10,"RAMER,SCOTT",2345
-11,"SPAQI,WILSON",2340
-12,"BAKER,VINCENT JIN",2320
-13,"HUGHES,JOHN LODGE",2314
-14,"AHLBORG,JOHN",2300
-15,"FENG,MAGGIE",2288
-16,"CAO,ELTON",2284
-17,"LIU,ARISTO S",2269
-18,"CLAYTON,RYAN DANI",2268
-19,"CHIRU,EDUARD",2248
-20,"WEAVER,BENJAMIN W",2238
-21,"MULTHOPP,HANS",2219
-22,"STORN,JUSTIN THOM",2214
-23,"KELLEY,DESHAWN",2213
-24,"KEUCHEL,DESTYNN",2212
-25,"SEWARD,STEVEN C",2211
-26,"MURTHY,PAPPU LN",2209
-27,"WRIGHT,WILLIAM B",2209
-28,"DIEBERT,CHARLES M",2207
-29,"BERCHENKO,SERGEY",2206
-30,"ALLEN,DAVID",2200
-31,"BENNETT,ALLAN D",2200
-32,"RANDOLPH,JOHN B",2200
-33,"JOELSON,MICHAEL",2188
-34,"WILSON,RUSSELL",2174
-35,"MODLIN,TREY",2169
-36,"JAFFE,JOEL GRANT",2153
-37,"STEEN,BOBBY",2153
-38,"SRIVASTAVA,VIKRAM",2145
-39,"BEATREZ,PATRICK L",2134
-40,"RAMASWAMY,ABHINAV",2129
-41,"TAN,RALPH",2117
-42,"CASDEN,ALAN",2112
-43,"MILLER,JOHN",2099
-44,"BALYAN,ARYAN SING",2097
-45,"PAGANINI,XAVIER N",2095
-46,"KOWALYSKO,ROMAN",2093
-47,"PASSEN,BRETT",2087
-48,"SAMPATH,AJA",2085
-49,"COHEN,JOHN",2077
-50,"ALLEN,ZACHARY JOH",2075`, {
+Papa.parse(`Ranking,Name,Rating,Grade
+1,"Cheng, Emma",1897,8
+2,"Mohanselvan, Adeti",1779,9
+3,"Prabu, Sneha",1721,10
+4,"Ma, Samantha",1657,12
+5,"Massick, Laine",1645,9
+6,"Harper, Alicia",1598,8
+7,"Tan, Patricia",1548,5
+8,"Babu, Arya",1464,7
+9,"Bagley, Hemma Svasti",1361,10
+10,"Paruchuri, Deepthisri",1225,7
+11,"Hoff-Miyazaki, Zenya",1198,12
+12,"Sharma, Arushi",1145,11
+13,"Rodgers, Greta",1133,10
+14,"Hodges, Alaiah",1121,10
+15,"Vollmar, Josi",1115,10
+16,"Seethiraju, Srikari",1070,6
+17,"Ganga, Harshita",1055,6
+18,"Chu, Kaylee",1042,6
+19,"Banks, Savannah",1000,9
+20,"Kolli, Manoghna",997,6
+21,"Lui, Jennifer",979,10
+22,"Bagley, Saadhika",976,4
+23,"Talla, Saumya",947,8
+24,"Gattikoppula, Bhuvandeer",912,9
+25,"Somakandan, Aizvereah",881,7`, {
 	complete: function(results) {
 		console.log("Finished:", results.data);
-		parseTop50(results.data);
-		// parseTop50Qualified(results.data);
+		// parseTop50(results.data);
+		parseTop50Qualified(results.data);
 		// parseOcaMembers(results.data);
 	}
 });
